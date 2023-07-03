a370c9db377688c86e8e5f466c647191d579664b
abhishek gaur <77878637+abhi-gaur@users.noreply.github.com>
2023-07-03 22:32:06 +0530

    Update Kubernetes_commands.sh

--- Kubernetes_commands.sh
+++ Kubernetes_commands.sh
@@ -2,3 +2,8 @@
 -> kubectl get pods -n=kube-public  // To find particular namespace
 
 -> kubectl create namespace django-todo-ns // to create new namespace
+
+-> kubectl delete -f pod.yml // to delete created pod
+-> kubectl apply -f _podfilename_   //to create a pod (after yml configuration file)
+
+-> kubectl get pods -o wide -n django-todo-ns // to view pods in wide option
