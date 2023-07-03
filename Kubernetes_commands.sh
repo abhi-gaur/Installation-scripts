552d96b87f6c786598033292e1ca9385ef488204
abhishek gaur <77878637+abhi-gaur@users.noreply.github.com>
2023-07-03 20:06:37 +0530

    Create Kubernetes_commands.sh

---
+++ Kubernetes_commands.sh
@@ -0,0 +1,4 @@
+-> Kubectl get namespaces // to Find All the namespaces
+-> kubectl get pods -ns=kube-public  // To find particular namespace
+
+-> kubectl create namespace django-todo-ns // to create new namespace
