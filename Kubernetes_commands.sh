637fa777ae2a2094e16a4b6cef4bf9535cda6fc4
abhishek gaur <77878637+abhi-gaur@users.noreply.github.com>
2023-07-07 20:18:09 +0530

    Update Kubernetes_commands.sh

--- Kubernetes_commands.sh
+++ Kubernetes_commands.sh
@@ -7,3 +7,5 @@
 -> kubectl apply -f _podfilename_   //to create a pod (after yml configuration file)
 
 -> kubectl get pods -o wide -n django-todo-ns // to view pods in wide option
+
+-> kubectl describe pods helloexample -n=node-app-ns   // to troubleshoot and too see description of each steps
